3000381e097a189a278ac7914ce11683658e27b2
Jason Santana <jsantana006@gmail.com>
2020-01-04 19:08:02 -0500

    added proper forwarding to the session

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,10 +18,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert', text: "The form contains 4 errors."
   end
 
-  test "successfull edit" do
-    log_in_as(@user)
+  test "successfull edit with friendly forwarding" do
     get edit_user_path(@user)
-    assert_template 'users/edit'
+    log_in_as(@user)
+    assert_redirected_to edit_user_path(@user)
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:   name,
