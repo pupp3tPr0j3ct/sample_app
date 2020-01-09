ec70f78bf9d28b2261d6ed08e53e085282b4d5b5
Jason Santana <jsantana006@gmail.com>
2020-01-08 19:04:50 -0500

    exercise 10.3.1; adding site layout tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,15 +3,28 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
  
   test "layout links" do
+    # Logged out link functionality
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", login_path
     get contact_path
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Sign up")
+    # Logged in link functionality
+    @user = users(:archer)
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", '#'
+    assert_select "a[href=?]", logout_path
+    # assert_select "title", full_title("Users")...looking to assert correct string
+    get users_path
+    assert_select "title", full_title("All users")
+    
   end
 end
