615cb9b37df89224f5c864492d988b6d22001e33
Jason Santana <pupp3tPr0j3ct@github.com>
2020-02-21 20:16:11 -0500

    sendgrid mailing functional

--- app/helpers/mail/sendgrid.rb
+++ app/helpers/mail/sendgrid.rb
@@ -11,4 +11,4 @@ sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 response = sg.client.mail._('send').post(request_body: mail.to_json)
 puts response.status_code
 puts response.body
-puts respose.headers
+puts response.headers
