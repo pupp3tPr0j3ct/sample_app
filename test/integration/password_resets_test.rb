432ae67193c3dbc7b8313fd172fb0ca9b79cee88
Jason Santana <pupp3tPr0j3ct@github.com>
2020-02-27 20:05:46 -0500

    password_reset_test correctly checks for invalid password & confirmation whereas before it had matching values, now it does not...it also checks for empty password and valid password and confirmation with an assertion for a nil reset digest.

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -42,8 +42,22 @@ class PasswordResetsTest < ActionDispatch::IntegrationTest
     patch password_reset_path(user.reset_token),
           params: { email: user.email,
                     user: { password:               "foobaz",
+                            password_confirmation:  "barquux" } }
+    assert_select 'div#error_explanation'
+    # Empty password
+    patch password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    user: { password:               "",
+                            password_confirmation:  "" } }
+    assert_select 'div#error_explanation'
+    # Valid password & confirmation
+    patch password_reset_path(user.reset_token),
+          params: { email: user.email,
+                    user: { password:               "foobaz",
                             password_confirmation:  "foobaz" } }
     assert is_logged_in?
+    user.reload
+    assert_nil user.reset_digest
     assert_not flash.empty?
     assert_redirected_to user
   end
