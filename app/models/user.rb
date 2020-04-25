db3fd87c79e47c77f3d371ebe29af52f424d3e31
Jason Santana <pupp3tPr0j3ct@github.com>
2020-04-25 15:04:32 -0400

    created a relationships model with validation and created a test to confirm validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,9 @@ class User < ApplicationRecord
     before_create   :create_activation_digest
 
     has_many        :microposts,    dependent: :destroy
+    has_many        :active_relationships, class_name: "Relationship",
+                                           foreign_key: "follower_id",
+                                           dependent:   :destroy
 
     validates :name, presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX =  /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
