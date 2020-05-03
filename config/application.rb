8bfe5645ce8e96c5723065ef8a568c8228e9dae8
Jason Santana <pupp3tPr0j3ct@github.com>
2020-05-02 20:51:03 -0400

    edited the relationships controller create and destroy actions to use AJAX, subsequently created destroy and create AJAX forms and edited the config app file for the helper token hashes.

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+require File.expand_path('../boot', __FILE__)
+
 module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    # Include the authenticity token in remote forms.
+    config.action_view_embed_authenticity_token_in_remote_forms = true
   end
 end
